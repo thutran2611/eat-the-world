02cf5238c216ab56af301f31550092be0331482a
Andrew Thornton <awjdthornton@gmail.com>
2019-07-17 16:58:21 -0700

    adding  missing STATIC settings

--- eat_the_World/config/base.py
+++ eat_the_World/config/base.py
@@ -113,7 +113,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # We also needed to add this to specify where the downloads go
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
