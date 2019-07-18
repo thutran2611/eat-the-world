5b45864301aff2df6c9e1e8d8b8dd31f0352900d
Andrew Thornton <awjdthornton@gmail.com>
2019-07-17 17:10:09 -0700

    take3 of fix COLLECTSTATIC problem

--- eat_the_World/config/base.py
+++ eat_the_World/config/base.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+#    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'apps/core'),
     os.path.join(BASE_DIR, 'apps/accounts'),
 )
