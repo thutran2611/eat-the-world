031c9f138275e8fb4c1e081555938b0feae1a660
Andrew Thornton <awjdthornton@gmail.com>
2019-07-19 16:53:20 -0700

    completing API code and list of cuisines to use as input for returning recipe

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.core import views
 
 urlpatterns = [
-    path('', views.test_page, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
 ]
