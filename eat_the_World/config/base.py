ee7d234754149186a12c8f918d122db902fb3276
rjplaton <rjplaton@gmail.com>
2019-07-22 09:42:37 -0700

    trying one more thing - setting static root to static

--- eat_the_World/config/base.py
+++ eat_the_World/config/base.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
