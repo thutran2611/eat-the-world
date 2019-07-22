534278b569030d33ceedf87a16a4dcab8c166229
rjplaton <rjplaton@gmail.com>
2019-07-21 20:24:24 -0700

    testing

--- eat_the_World/config/base.py
+++ eat_the_World/config/base.py
@@ -113,15 +113,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+#STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, '../apps/core'),
-    os.path.join(BASE_DIR, '../apps/accounts'),
-)
+#os.path.join(BASE_DIR, '../apps/core'),
+#os.path.join(BASE_DIR, '../apps/accounts'),
+#)
 
 # We also needed to add this to specify where the downloads go
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
