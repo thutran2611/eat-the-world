37362d73683ecfface75cb727d6e93f16418beff
Andrew Thornton <awjdthornton@gmail.com>
2019-07-22 15:03:21 -0700

    turning debug back on to troubleshoot staticfiles issue

--- eat_the_World/config/production.py
+++ eat_the_World/config/production.py
@@ -1,11 +1,11 @@
 # Settings that are unique to production go here
 from .base import *  # noqa
 
-DEBUG = True
+DEBUG = False
 
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'eat-the-world.herokuapp.com'
-]
+#ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'eat-the-world.herokuapp.com'
+#]
