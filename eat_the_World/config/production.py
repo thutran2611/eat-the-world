5748641516c268d71c47c8f27f55c9c65f10d41d
rjplaton <rjplaton@gmail.com>
2019-07-21 21:49:51 -0700

    setting debug true again

--- eat_the_World/config/production.py
+++ eat_the_World/config/production.py
@@ -1,10 +1,18 @@
 # Settings that are unique to production go here
 from .base import *  # noqa
 
-DEBUG = False
+DEBUG = True
 
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'eat-the-world.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'eat-the-world.herokuapp.com'
+]
+
+INSTALLED_APPS += ['debug_toolbar']
+
+# Add in Debug Toolbar Middleware
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+] + MIDDLEWARE
