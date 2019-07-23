cca5f6107348f604121eeaff6b12c838777206f5
Andrew Thornton <awjdthornton@gmail.com>
2019-07-22 18:19:00 -0700

    very verbose lgoging

--- eat_the_world/config/production.py
+++ eat_the_world/config/production.py
@@ -8,3 +8,20 @@ DEBUG = False
 import django_heroku
 django_heroku.settings(locals())
 
+import os
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
