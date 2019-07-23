d8771066ecb6b2296a3d85996fe85db6c5b9f057
Andrew Thornton <awjdthornton@gmail.com>
2019-07-22 18:11:17 -0700

    fliipping debug

--- eat_the_world/config/production.py
+++ eat_the_world/config/production.py
@@ -1,7 +1,8 @@
 # Settings that are unique to production go here
 from .base import *  # noqa
 
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 # Configure Django App for Heroku.
 import django_heroku
